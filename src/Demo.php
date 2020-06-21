164775ce0f0449494049934715eb79c8088c479f
phpmorphy <47130682+phpmorphy@users.noreply.github.com>
2020-06-21 19:46:14 +1200

    Update Demo.php

--- src/Demo.php
+++ src/Demo.php
@@ -7,7 +7,7 @@ class Demo
     public function testMe($a, $b)
     {
         if (function_exists('sodium_crypto_sign_seed_keypair')) {
-            sodium_crypto_sign_seed_keypair(str_repeat("\x0", 32))
+            sodium_crypto_sign_seed_keypair(str_repeat("\x0", 32));
         }
         
         return $a + $b;
